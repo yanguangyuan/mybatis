872bca2ce89c57bb107ab31c547ffc312ad01dd9
yanguangyuan <1847344456@qq.com>
2020-01-06 17:49:41 +0800

    解析xml datasource

--- src/main/java/com/ygy/learn/mybatis/entity/Configuration.java
+++ src/main/java/com/ygy/learn/mybatis/entity/Configuration.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Map;
 
 /**
@@ -20,5 +23,15 @@ public class Configuration {
      * 所有配置的mapper.xml的标签信息，key = namespace+id;value = 信息
      */
     private Map<String,MappedStatement> mappedStatements;
+    private DataSource dataSource;
+
+    /**
+     * 获取连接
+     * @return
+     * @throws SQLException
+     */
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
 
 }
--- src/test/java/com/ygy/learn/mybatis/jdbc/ConnectTest.java
+++ src/test/java/com/ygy/learn/mybatis/jdbc/ConnectTest.java
@@ -1,16 +1,23 @@
 package com.ygy.learn.mybatis.jdbc;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.ygy.learn.mybatis.connect.ConnectFactory;
 import com.ygy.learn.mybatis.connect.ConnectParam;
+import com.ygy.learn.mybatis.entity.Configuration;
 import com.ygy.learn.mybatis.utils.FileReadUtil;
 import com.ygy.learn.mybatis.utils.MapToBeanUtil;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.dom4j.Element;
+import org.dom4j.Node;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
 
 /**
  * @author : yanguangyuan
@@ -25,7 +32,7 @@ public class ConnectTest {
     @BeforeEach
     void init() throws Exception {
         String path = getClass().getResource("/db.properties").getPath();
-        connectParam = MapToBeanUtil.convert(FileReadUtil.readProperties(path),ConnectParam.class);
+        connectParam = MapToBeanUtil.convert(FileReadUtil.readProperties(path), ConnectParam.class);
     }
 
     @Test
@@ -43,4 +50,98 @@ public class ConnectTest {
         connection.close();
 
     }
+
+    @Test
+    void test() throws Exception{
+        Configuration configuration = loadConfiguration();
+        Connection connection = configuration.getConnection();
+        String sql = "select * from user where id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, "136a07f8824f4769a3e7593b762d5d48");
+        ResultSet set = statement.executeQuery();
+        while (set.next()) {
+            log.info(set.getString("username"));
+        }
+        statement.close();
+        connection.close();
+    }
+
+    /**
+     * 加载配置
+     *
+     * @return
+     */
+    private Configuration loadConfiguration() {
+        Configuration configuration = new Configuration();
+        //读取xml配置
+        String path = getClass().getResource("/mybatis-config.xml").getPath();
+        Element element = FileReadUtil.readXml(path);
+        Element environments = element.element("environments");
+        handleEnvironments(configuration, environments);
+        Element mappers = element.element("mappers");
+        handleMappers(configuration, mappers);
+        return configuration;
+    }
+
+    /**
+     * 处理环境参数
+     *
+     * @param configuration
+     * @param environments
+     */
+    private void handleEnvironments(Configuration configuration, Element environments) {
+        Element environment = environments.element("environment");
+        handleDataSource(configuration, environment);
+    }
+
+    /**
+     * 处理datasource
+     *
+     * @param configuration
+     * @param environment
+     */
+    private void handleDataSource(Configuration configuration, Element environment) {
+        Element dsElement = environment.element("dataSource");
+        String type = dsElement.attributeValue("type");
+
+        String driver = "";
+        String url = "";
+        String username = "";
+        String password = "";
+        if ("druid".equals(type)) {
+            List<Element> properties = dsElement.elements();
+            int count = properties.size();
+            for (int i = 0; i < count; i++) {
+                Element node =  properties.get(i);
+                String name = node.attributeValue("name");
+                String value = node.attributeValue("value");
+                log.info(name+"--"+value);
+                if ("driver".equals(name)) {
+                    driver = value;
+                } else if ("url".equals(name)) {
+                    url = value;
+                } else if ("username".equals(name)) {
+                    username=value;
+                } else if ("password".equals(name)) {
+                    password=value;
+                }
+            }
+            DruidDataSource dataSource =new DruidDataSource();
+            dataSource.setDriverClassName(driver);
+            dataSource.setUrl(url);
+            dataSource.setUsername(username);
+            dataSource.setPassword(password);
+            configuration.setDataSource(dataSource);
+        }
+    }
+
+    /**
+     * 处理mapper
+     *
+     * @param configuration
+     * @param mappers
+     */
+    private void handleMappers(Configuration configuration, Element mappers) {
+    }
+
 }
